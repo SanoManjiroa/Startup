b4b1b0888571af6de6a13242369cb02c4c0503cc
SanoManjiroa <hrmanagerprogress@gmail.com>
2025-07-20 17:46:17 +0500

    Css bo'yicha ba'zi xossalarni test qilib chiqayabman static dir tuzildi va dasturga ulandi

--- Lison/settings.py
+++ Lison/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Path to your global static folder
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
